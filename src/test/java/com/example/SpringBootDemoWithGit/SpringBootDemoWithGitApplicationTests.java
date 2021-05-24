d2508f72cabdb825bd1128db725b77752733757c
Ajieshmon <ajieshmon@gmail.com>
2021-05-24 10:38:53 +0530

    Project Setup By Ajiesh

---
+++ src/test/java/com/example/SpringBootDemoWithGit/SpringBootDemoWithGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootDemoWithGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoWithGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
